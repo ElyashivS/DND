11d855a242aa5b7e53cf4030bf4085e8ecaf1196
Elyashiv <Elyashiv2468@gmail.com>
2021-06-02 16:19:43 +0300

    Added Board class as recommended in PS

---
+++ src/Board/Board.java
@@ -0,0 +1,14 @@
+package Board;
+
+import Enemies.Enemy;
+import Players.Player;
+import Tiles.Tile;
+
+import java.util.List;
+
+public class Board {
+    Player player;
+    List<Enemy> enemiesList;
+    List<Tile> tiles;
+
+}
