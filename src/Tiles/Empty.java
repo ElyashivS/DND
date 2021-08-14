5329f6e8d199b3744f86b8524afef2b10aa1ba86
ElyashivS <73721975+ElyashivS@users.noreply.github.com>
2021-08-14 17:33:03 +0300

    Delete src/Tiles directory

--- src/Tiles/Empty.java
+++
@@ -1,9 +0,0 @@
-package Tiles;
-
-import Board.Point;
-
-public class Empty extends Tile{
-    public Empty(char tile, Point position) {
-        super('.', position);
-    }
-}
--- src/Tiles/Wall.java
+++
@@ -1,9 +0,0 @@
-package Tiles;
-
-import Board.Point;
-
-public class Wall extends Tile {
-    public Wall(char tile, Point position) {
-        super('#', position);
-    }
-}
